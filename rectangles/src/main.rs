eb072ddd488c0abb704ce98db9ee64bbdc164872
tomocy <tomocy.dev@gmail.com>
2020-05-19 15:44:22 +0900

    rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    let rect_a = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect_b = Rectangle::square(10);
+    let rect_c = Rectangle::square(10);
+
+    println!("rectangle a: {:#?}", rect_a);
+    println!("rectangle b: {:#?}", rect_b);
+    println!("rectangle c: {:#?}", rect_c);
+    println!("The area of rectangle a is: {}.", rect_a.area());
+    println!("The area of rectangle b is: {}.", rect_b.area());
+    println!("The area of rectangle c is: {}.", rect_c.area());
+    println!(
+        "Can rectangle a hold rectangle b?: {}",
+        rect_a.can_hold(&rect_b),
+    );
+    println!(
+        "Can rectangle b hold rectangle c?: {}",
+        rect_b.can_hold(&rect_c),
+    );
+    println!(
+        "Can rectangle c hold rectangle a?: {}",
+        rect_c.can_hold(&rect_a),
+    );
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
