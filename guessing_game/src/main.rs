c8089d42157cb662815fc8904e629306a10ceab2
tomocy <tomocy.dev@gmail.com>
2020-05-20 17:10:54 +0900

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,22 +15,42 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(number) => number,
+        let guess = match guess.trim().parse() {
+            Ok(num) => guess::Guess::new(num),
             Err(_) => {
                 println!("Please type a number!");
                 continue;
             }
         };
 
-        println!("You guessed: {}", guess);
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
+        }
+    }
+}
+
+mod guess {
+    pub struct Guess {
+        value: u32,
+    }
+
+    impl Guess {
+        pub fn new(value: u32) -> Guess {
+            if value < 1 || 100 < value {
+                panic!("Guess value must be between 1 and 100.");
+            }
+
+            Guess { value }
+        }
+
+        pub fn value(&self) -> u32 {
+            self.value
         }
     }
 }
