63620b3f551a621066361d7f7952939aebef8da4
tomocy <tomocy.dev@gmail.com>
2020-05-21 12:24:06 +0900

    training plan

---
+++ training_plan/src/main.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+use std::thread;
+use std::time;
+
+fn main() {
+    let intensity = 10;
+    let random = 4;
+
+    generate_work(intensity, random);
+}
+
+fn generate_work(intensity: u32, random: u32) {
+    let mut calculate_target_level = Cacher::new(simulated_expensive_calculation);
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            calculate_target_level.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            calculate_target_level.value(intensity)
+        );
+
+        return;
+    }
+
+    if random == 3 {
+        println!("Take a break today! Remember to stay hydrated!");
+        return;
+    }
+
+    println!(
+        "Today, run for {} minutes!",
+        calculate_target_level.value(intensity)
+    );
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(time::Duration::from_secs(2));
+    intensity
+}
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculate: T,
+    values: HashMap<u32, u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculate: T) -> Cacher<T> {
+        Cacher {
+            calculate: calculate,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, x: u32) -> u32 {
+        match self.values.get(&x) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculate)(x);
+                self.values.insert(x, v);
+                v
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cache_different_values() {
+        let mut cacher = Cacher::new(|x| x);
+
+        cacher.value(1);
+        cacher.value(2);
+        assert_eq!(1, cacher.value(1));
+        assert_eq!(2, cacher.value(2));
+    }
+}
