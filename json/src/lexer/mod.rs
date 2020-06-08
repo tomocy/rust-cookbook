39bc05fe99f21c4785efaab63459b65617b8917f
tomocy <tomocy.dev@gmail.com>
2020-06-08 14:32:48 +0900

    update Lexer
    Lexer can read number.

--- json/src/lexer/mod.rs
+++ json/src/lexer/mod.rs
@@ -18,6 +18,7 @@ impl Lexer {
     pub fn read_token(&mut self) -> Token {
         match self.current_char() {
             "\x00" => self.compose(Token::EOF),
+            _ if self.do_have_number() => self.compose_number(),
             _ => self.compose(Token::Unknown),
         }
     }
@@ -25,6 +26,24 @@ impl Lexer {
     pub fn compose(&mut self, token: Token) -> Token {
         self.read_char();
         token
+    }
+
+    fn compose_number(&mut self) -> Token {
+        Token::Number(self.read_number())
+    }
+
+    fn read_number(&mut self) -> u32 {
+        let begin = self.index;
+        while self.do_have_number() {
+            self.read_char();
+        }
+
+        self.src[begin..self.index].parse().unwrap()
+    }
+
+    fn do_have_number(&self) -> bool {
+        let curr = self.current_char();
+        "0" <= curr && curr <= "9"
     }
 
     fn current_char(&self) -> &str {
@@ -48,4 +67,5 @@ impl Lexer {
 pub enum Token {
     Unknown,
     EOF,
+    Number(u32),
 }
