5dc4bd6af8db3cbf60488d3e3ec55c80f9106296
tomocy <tomocy.dev@gmail.com>
2020-06-08 15:01:19 +0900

    add Parser::new

--- json/src/parser/mod.rs
+++ json/src/parser/mod.rs
@@ -1,4 +1,14 @@
 #[cfg(test)]
 mod tests;
 
-pub struct Parser {}
+use super::lexer;
+
+pub struct Parser {
+    lexer: lexer::Lexer,
+}
+
+impl Parser {
+    pub fn new(lexer: lexer::Lexer) -> Self {
+        Self { lexer }
+    }
+}
