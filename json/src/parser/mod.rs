f87e1ffc3a07ff96216c737789a39b2f01618bf6
tomocy <tomocy.dev@gmail.com>
2020-06-08 17:17:30 +0900

    modify Parser

--- json/src/parser/mod.rs
+++ json/src/parser/mod.rs
@@ -4,6 +4,8 @@ mod tests;
 use super::lexer;
 use std::error;
 
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
 #[derive(Debug)]
 pub struct Parser {
     lexer: lexer::Lexer,
@@ -25,61 +27,69 @@ impl Parser {
         p
     }
 
-    pub fn parse(&mut self) -> Result<Value, Box<dyn error::Error>> {
+    pub fn parse(&mut self) -> Result<Value> {
         match &self.curr_token {
             lexer::Token::EOF => Err(From::from("input should not be empty")),
-            lexer::Token::LBrace => Ok(self.parse_object()),
-            lexer::Token::Number(_) => Ok(self.parse_number()),
-            lexer::Token::String(_) => Ok(self.parse_string()),
+            lexer::Token::LBrace => self.parse_object(),
+            lexer::Token::Number(_) => self.parse_number(),
+            lexer::Token::String(_) => self.parse_string(),
             lexer::Token::Unknown(s) => Err(From::from(format!("token '{}' is unknown", s))),
             _ => Err(From::from("not implemented")),
         }
     }
 
-    fn parse_object(&mut self) -> Value {
+    fn parse_object(&mut self) -> Result<Value> {
         debug_assert_eq!(lexer::Token::LBrace, self.curr_token);
         self.read_token();
 
         let mut props = Vec::new();
 
         if self.do_have_token(lexer::Token::RBrace) {
-            return Value::Object(props);
+            return Ok(Value::Object(props));
         }
 
         loop {
-            let prop = self.parse_property();
+            let prop = self.parse_property()?;
             props.push(prop);
 
-            if !self.do_have_token(lexer::Token::Comma) || self.do_have_token(lexer::Token::EOF) {
+            if self.do_have_token(lexer::Token::EOF) {
+                return Err(From::from("object should be closed with '}'"));
+            }
+
+            if !self.do_have_token(lexer::Token::Comma) {
                 break;
             }
 
             self.read_token();
         }
 
-        Value::Object(props)
+        Ok(Value::Object(props))
     }
 
-    fn parse_property(&mut self) -> Property {
-        let key = if let Value::String(s) = self.parse_string() {
+    fn parse_property(&mut self) -> Result<Property> {
+        let key = if let Ok(Value::String(s)) = self.parse_string() {
             s
         } else {
             panic!("key should be string");
         };
 
-        debug_assert_eq!(lexer::Token::Colon, self.curr_token);
+        if !self.do_have_token(lexer::Token::Colon) {
+            return Err(From::from(
+                "property should be composed of key, ':', and value",
+            ));
+        }
         self.read_token();
 
         let value = self.parse().unwrap();
 
-        Property::new(&key, value)
+        Ok(Property::new(&key, value))
     }
 
-    fn parse_number(&mut self) -> Value {
+    fn parse_number(&mut self) -> Result<Value> {
         let n = self.read_number();
         self.read_token();
 
-        n
+        Ok(n)
     }
 
     fn read_number(&self) -> Value {
@@ -89,11 +99,11 @@ impl Parser {
         }
     }
 
-    fn parse_string(&mut self) -> Value {
+    fn parse_string(&mut self) -> Result<Value> {
         let s = self.read_string();
         self.read_token();
 
-        s
+        Ok(s)
     }
 
     fn read_string(&self) -> Value {
