2d1970e86bceef0628443236ab4a164c1a012248
tomocy <tomocy.dev@gmail.com>
2020-06-08 22:06:02 +0900

    update game of life
    Universe stop coping the string of itself from Rust to JS but pass its cells with wasm memory.

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -36,6 +36,18 @@ impl Universe {
         }
     }
 
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     pub fn render(&self) -> String {
         self.to_string()
     }
