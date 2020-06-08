19f8bb81e1cc0ed6ba3841b604d80dc5eb0edc0d
tomocy <tomocy.dev@gmail.com>
2020-06-08 17:09:51 +0900

    update Parser
    Parser can parse object.

--- json/src/parser/mod.rs
+++ json/src/parser/mod.rs
@@ -28,11 +28,51 @@ impl Parser {
     pub fn parse(&mut self) -> Result<Value, Box<dyn error::Error>> {
         match &self.curr_token {
             lexer::Token::EOF => Err(From::from("input should not be empty")),
+            lexer::Token::LBrace => Ok(self.parse_object()),
             lexer::Token::Number(_) => Ok(self.parse_number()),
             lexer::Token::String(_) => Ok(self.parse_string()),
             lexer::Token::Unknown(s) => Err(From::from(format!("token '{}' is unknown", s))),
             _ => Err(From::from("not implemented")),
         }
+    }
+
+    fn parse_object(&mut self) -> Value {
+        debug_assert_eq!(lexer::Token::LBrace, self.curr_token);
+        self.read_token();
+
+        let mut props = Vec::new();
+
+        if self.do_have_token(lexer::Token::RBrace) {
+            return Value::Object(props);
+        }
+
+        loop {
+            let prop = self.parse_property();
+            props.push(prop);
+
+            if !self.do_have_token(lexer::Token::Comma) || self.do_have_token(lexer::Token::EOF) {
+                break;
+            }
+
+            self.read_token();
+        }
+
+        Value::Object(props)
+    }
+
+    fn parse_property(&mut self) -> Property {
+        let key = if let Value::String(s) = self.parse_string() {
+            s
+        } else {
+            panic!("key should be string");
+        };
+
+        debug_assert_eq!(lexer::Token::Colon, self.curr_token);
+        self.read_token();
+
+        let value = self.parse().unwrap();
+
+        Property::new(&key, value)
     }
 
     fn parse_number(&mut self) -> Value {
@@ -63,6 +103,10 @@ impl Parser {
         }
     }
 
+    fn do_have_token(&self, token: lexer::Token) -> bool {
+        self.curr_token == token
+    }
+
     fn read_token(&mut self) {
         self.curr_token = self.next_token.clone();
         self.next_token = self.lexer.read_token();
@@ -71,6 +115,22 @@ impl Parser {
 
 #[derive(Debug, PartialEq)]
 pub enum Value {
+    Object(Vec<Property>),
     Number(u32),
     String(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Property {
+    key: String,
+    value: Value,
+}
+
+impl Property {
+    pub fn new(key: &str, value: Value) -> Self {
+        Self {
+            key: key.to_string(),
+            value,
+        }
+    }
 }
--- json/src/parser/tests.rs
+++ json/src/parser/tests.rs
@@ -50,3 +50,59 @@ fn parse_string() {
         assert_eq!(actual, expected);
     }
 }
+
+#[test]
+fn parse_empty_object() {
+    let src = "{}";
+    let expected = vec![parser::Value::Object(vec![])];
+
+    let lexer = lexer::Lexer::new(src);
+    let mut parser = parser::Parser::new(lexer);
+
+    for expected in expected {
+        let actual = parser.parse().expect("should have succeeded to parse");
+
+        assert_eq!(actual, expected);
+    }
+}
+
+#[test]
+fn parse_object_with_single_property() {
+    let src = r#"{"aiueo": 12345}"#;
+    let expected = vec![parser::Value::Object(vec![parser::Property::new(
+        "aiueo",
+        parser::Value::Number(12345),
+    )])];
+
+    let lexer = lexer::Lexer::new(src);
+    let mut parser = parser::Parser::new(lexer);
+
+    for expected in expected {
+        let actual = parser.parse().expect("should have succeeded to parse");
+
+        assert_eq!(actual, expected);
+    }
+}
+
+#[test]
+fn parse_object_with_multiple_properties() {
+    let src = r#"{
+    "aaa": 111,
+    "bbb": 222,
+    "ccc": 333
+}"#;
+    let expected = vec![parser::Value::Object(vec![
+        parser::Property::new("aaa", parser::Value::Number(111)),
+        parser::Property::new("bbb", parser::Value::Number(222)),
+        parser::Property::new("ccc", parser::Value::Number(333)),
+    ])];
+
+    let lexer = lexer::Lexer::new(src);
+    let mut parser = parser::Parser::new(lexer);
+
+    for expected in expected {
+        let actual = parser.parse().expect("should have succeeded to parse");
+
+        assert_eq!(actual, expected);
+    }
+}
