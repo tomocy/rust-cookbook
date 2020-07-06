e13c1eb7d1ac399ac90bae512af1b1d92625f00c
tomocy <tomocy.dev@gmail.com>
2020-07-06 23:17:28 +0900

    tcp_echo_server: update Server
    Server logs.

--- tcp_echo_server/src/lib.rs
+++ tcp_echo_server/src/lib.rs
@@ -13,41 +13,66 @@ struct Server {
 }
 
 impl Server {
+    const BUF_SIZE: usize = 1024;
+
     fn new(address: String) -> Self {
         Self { address }
     }
 
     fn listen_and_serve(&self) -> Result<(), Box<dyn error::Error>> {
         let listener = net::TcpListener::bind(&self.address)?;
-        println!("listening on {}", self.address);
+        Self::log_info(&format!("listening on {}", self.address));
 
         for stream in listener.incoming() {
             let stream = match stream {
                 Ok(stream) => stream,
                 Err(err) => {
-                    eprintln!("failed to accept a stream: {}", err);
+                    Self::log_error(&format!("failed to accept a stream: {}", err));
                     continue;
                 }
             };
 
-            let work = self.spawn_worker();
+            Self::log_debug("accept a stream");
+
+            let work = Self::spawn();
             thread::spawn(move || {
                 if let Err(err) = work(stream) {
-                    eprintln!("faield to serve a stream: {}", err);
+                    Self::log_error(&format!("failed to work: {}", err));
                 }
+
+                Self::log_debug("succeed to work");
             });
+
+            Self::log_debug("accept a stream");
         }
 
         Ok(())
     }
 
-    fn spawn_worker(
-        &self,
-    ) -> Box<dyn Fn(net::TcpStream) -> Result<(), Box<dyn error::Error>> + Send> {
+    fn spawn() -> Box<dyn Fn(net::TcpStream) -> Result<(), Box<dyn error::Error>> + Send> {
         Box::new(|mut stream: net::TcpStream| loop {
-            let mut buf = [0; 1024];
+            let mut buf = [0; Self::BUF_SIZE];
             let n = stream.read(&mut buf)?;
             stream.write(&buf[0..n])?;
+            if n < Self::BUF_SIZE {
+                return Ok(());
+            }
         })
+    }
+
+    fn log_info(msg: &str) {
+        Self::log("INFO", msg);
+    }
+
+    fn log_debug(msg: &str) {
+        Self::log("DEBUG", msg);
+    }
+
+    fn log_error(msg: &str) {
+        Self::log("ERROR", msg);
+    }
+
+    fn log(level: &str, msg: &str) {
+        println!("[{}] {}", level, msg);
     }
 }
