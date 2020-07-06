747a658b1f356a9f5c0bba5124ae3d7002d8ff0a
tomocy <tomocy.dev@gmail.com>
2020-07-06 22:25:49 +0900

    tcp_echo_server: add Server

--- tcp_echo_server/src/lib.rs
+++ tcp_echo_server/src/lib.rs
@@ -3,3 +3,13 @@ use std::error;
 pub fn run() -> Result<(), Box<dyn error::Error>> {
     Err(From::from("not implemented"))
 }
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Self { address }
+    }
+}
