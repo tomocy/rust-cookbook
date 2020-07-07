eff09aa2417e8ffaa2d4abd4c039c2b4483a5113
tomocy <tomocy.dev@gmail.com>
2020-07-07 15:51:35 +0900

    http_echo_server: update ParseResult
    ParseResult can be from a result.

--- http_echo_server/src/lib.rs
+++ http_echo_server/src/lib.rs
@@ -10,4 +10,13 @@ enum ParseResult<'a, E> {
     Err(E),
 }
 
+impl<'a, E> From<Result<Request<'a>, E>> for ParseResult<'a, E> {
+    fn from(result: Result<Request<'a>, E>) -> Self {
+        match result {
+            Ok(req) => ParseResult::Ok(req),
+            Err(err) => ParseResult::Err(err),
+        }
+    }
+}
+
 struct Request<'a>(&'a str);
