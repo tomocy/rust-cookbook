486dfbc8e4e46da65ae087303a5939d873503868
tomocy <tomocy.dev@gmail.com>
2020-07-07 15:46:32 +0900

    http_echo_server: add ParseResult

--- http_echo_server/src/lib.rs
+++ http_echo_server/src/lib.rs
@@ -4,4 +4,10 @@ pub fn run() -> Result<(), Box<dyn error::Error>> {
     Err(From::from("not implemented"))
 }
 
+enum ParseResult<'a, E> {
+    Ok(Request<'a>),
+    Continuing,
+    Err(E),
+}
+
 struct Request<'a>(&'a str);
