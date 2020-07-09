1cf0c3221d463cfc8c400d01dd6401ca5017a3e6
tomocy <tomocy.dev@gmail.com>
2020-07-10 00:25:39 +0900

    monkey: add Lexer

--- monkey/src/lib.rs
+++ monkey/src/lib.rs
@@ -1,7 +1,18 @@
 use std::error;
+use std::io;
 
 pub fn run<T: Iterator<Item = String>>(_: T) -> Result<(), Box<dyn error::Error>> {
     Err("not implemented".into())
+}
+
+struct Lexer {
+    src_reader: Box<dyn io::Read>,
+}
+
+impl Lexer {
+    fn new(src_reader: Box<dyn io::Read>) -> Self {
+        Self { src_reader }
+    }
 }
 
 enum Token {
