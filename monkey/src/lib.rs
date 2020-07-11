bcde2b824be9ef4e75854666c65e51e0776adf96
tomocy <tomocy.dev@gmail.com>
2020-07-12 00:00:10 +0900

    monkey: update Lexer
    Lexer can read a int.

--- monkey/src/lib.rs
+++ monkey/src/lib.rs
@@ -27,8 +27,19 @@ impl<'src> Lexer<'src> {
         let ch = self.char();
         match ch {
             Self::EOF => Token::EOF,
+            _ if self.have_digit() => Token::Int(self.read_number()),
             _ => Token::Illegal(String::from_utf8(vec![ch]).unwrap()),
         }
+    }
+
+    fn read_number(&mut self) -> i32 {
+        let begin = self.pos;
+
+        while self.have_digit() {
+            self.read_char();
+        }
+
+        self.src[begin..self.pos].parse().unwrap()
     }
 
     fn read_char(&mut self) {
@@ -38,6 +49,11 @@ impl<'src> Lexer<'src> {
 
         self.pos = self.reading_pos;
         self.reading_pos += 1;
+    }
+
+    fn have_digit(&self) -> bool {
+        let ch = self.char();
+        b'0' <= ch && ch <= b'9'
     }
 
     fn char(&self) -> u8 {
@@ -53,6 +69,7 @@ impl<'src> Lexer<'src> {
 enum Token {
     Illegal(String),
     EOF,
+    Int(i32),
 }
 
 #[cfg(test)]
@@ -65,6 +82,19 @@ mod tests {
         let mut lexer = Lexer::new(src);
 
         let expected = vec![Token::EOF];
+
+        for expected in expected.into_iter() {
+            let actual = lexer.read_token();
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn lexer_reads_int() {
+        let src = "12345";
+        let mut lexer = Lexer::new(src);
+
+        let expected = vec![Token::Int(12345), Token::EOF];
 
         for expected in expected.into_iter() {
             let actual = lexer.read_token();
